f3a1bc1510018cf18c3d915703c4823652db5eae
skul-hub <yoelafrido42@gmail.com>
2025-11-11 11:45:51 +0700

    Create layout.tsx

---
+++ src/app/layout.tsx
@@ -0,0 +1,24 @@
+import { Inter } from 'next/font/google';
+import './globals.css';
+import { AuthProvider } from '@/components/AuthProvider';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata = {
+  title: 'BANGSKULL',
+  description: 'Streetwear Shop',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <AuthProvider>{children}</AuthProvider>
+      </body>
+    </html>
+  );
+}
