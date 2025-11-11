303136131d08ebca1678b87350c8674a3cfcaa8a
skul-hub <yoelafrido42@gmail.com>
2025-11-11 12:06:46 +0700

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://iwdmusemrlavcehuslbb.supabase.co';
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml3ZG11c2VtcmxhdmNlaHVzbGJiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI4MzQ5MDksImV4cCI6MjA3ODQxMDkwOX0.4-a_63iOU6XRxWzs_ueUuKhRT0CKGevIQ9_jE3C5Vt8';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
